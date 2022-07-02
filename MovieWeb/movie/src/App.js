88b232ce7d0d6675c48ac2db9942797a082d2b56
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-07-02 21:48:11 +0900

    저장

---
+++ MovieWeb/movie/src/App.js
@@ -0,0 +1,14 @@
+import Button from "./Button";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <h1>Welcome back!</h1>
+        <Button text={"continue"}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ MovieWeb/movie/src/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import propTypes from "prop-types";
+
+const Button = ({ text }) => {
+  return <div>{text}</div>;
+};
+
+Button.propTypes={
+    text: propTypes.string.isRequired
+}
+
+export default Button;
